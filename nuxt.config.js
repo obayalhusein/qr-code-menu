1b98ced15be92c8b4c40223bc0a951aa9ef58483
obayalhusein <obayalhusein@gmail.com>
2024-02-24 01:22:22 +0300

    change tab title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - qr-menu',
-    title: 'qr-menu',
+    titleTemplate: '%s - حكايا الفنجان',
+    title: 'قائمة',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
