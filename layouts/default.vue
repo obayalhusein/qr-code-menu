bcba4eb3a7b9cf600fb0bd1c372a38a1aa399337
obayalhusein <obayalhusein@gmail.com>
2024-02-24 03:03:50 +0300

    change logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
   <v-app dir="rtl">
     <v-app-bar fixed app>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-toolbar-title>
+        <v-img
+          src="/images/logo/text-and-symbol.svg"
+          height="40"
+          width="220"
+          contain
+        />
+      </v-toolbar-title>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
